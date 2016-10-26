5e2943acd0e6c55171f480894d449de65dc4c817
yorickdewid <yorick17@outlook.com>
2016-10-26 22:05:56 +0200

    Generate pre-keys from curve

--- src/KeyHelper.h
+++ src/KeyHelper.h
@@ -6,15 +6,39 @@
 
 namespace signalpp {
 
-namespace KeyHelper {
+class KeyHelper {
+	signal_context *context;
 
-static const short generateRegistrationId() {
-    unsigned char *randId = (unsigned char *)CryptoProvider::getRandomBytes(1);
-    unsigned short int registrationId = (short)(((unsigned char)*randId) << 8);
-    return registrationId & 0x3fff;
-}
+public:
+	KeyHelper() {
+		signal_context_create(&context, 0);//TODO: move
 
-} // namespace KeyHelper
+		CryptoProvider::hook(context);
+	}
+
+	~KeyHelper() {
+		signal_context_destroy(context);//TODO: move
+	}
+
+	static const short generateRegistrationId() {
+		unsigned char *randId = (unsigned char *)CryptoProvider::getRandomBytes(1);
+		unsigned short int registrationId = (short)(((unsigned char)*randId) << 8);
+		return registrationId & 0x3fff;
+	}
+
+	ec_key_pair *generatePreKey() {
+		ec_key_pair *key_pair;
+
+		int result = curve_generate_key_pair(context, &key_pair);
+		if (result) {
+			SIGNAL_LOG_ERROR << "curve_generate_key_pair() failed";
+			return nullptr; //TODO: throw
+		}
+
+		return key_pair;
+	}
+
+};
 
 } // namespace signalpp
 
